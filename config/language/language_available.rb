6523a58bc70cbb72e9f17c38457464c4e9744238
Rodrigo <rodrigosiqueiramelo@gmail.com>
2014-12-27 12:04:00 -0200

    Update .travis file and documentation.

--- config/language/language_available.rb
+++ config/language/language_available.rb
@@ -1,6 +1,7 @@
 module Configuration
+  #Handling the languages available in the system.
   class Language_Available
     public
-      LANGUAGES = ["ruby", "python", "c++", "c"]
+      LANGUAGES = ["ruby", "python", "c++", "c"] #Put here new language.
   end
 end
--- config/monitor/monitor_available.rb
+++ config/monitor/monitor_available.rb
@@ -1,6 +1,13 @@
+# Configuration module keep all the constants for make the system works.
+# | __Configure__ | __Description__ |
+# |---|---|
+# | Monitor  | Types of monitoring available. |
+# | Language | Languages available. |
+# | Watchdog | Types of watchdog. |
 module Configuration
+  # Configuration for the monitor available.
   class Monitor_Available
     public
-      MONITORS = ["uml", "traceability"]
+      MONITORS = ["uml", "traceability"] #List with monitors types.
   end
 end
