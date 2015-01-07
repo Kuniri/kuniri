b83a661083ed5d6333b4e826d5aea614e454924e
Rodrigo <rodrigosiqueiramelo@gmail.com>
2015-01-07 10:25:30 -0200

    Improve ruby_language

--- app/language/language.rb
+++ app/language/language.rb
@@ -11,31 +11,31 @@ module Languages
  
       # Extract all the comments from the source.
       # @param source [String] Source code to analys.
-      def comment_extract(source)
+      def comment_extract
         raise NotImplementedError
       end
 
       # Extract all the method/function from the source.
       # @param source [String]
-      def method_extract(source)
+      def method_extract
         raise NotImplementedError
       end
 
       # Extract all the class declared in the source.
       # @param source [String]
-      def class_extract(source)
+      def class_extract
         raise NotImplementedError
       end
 
       # Extract the attribute from source file.
       # @param source [String]
-      def attribute_extract(source)
+      def attribute_extract
         raise NotImplementedError
       end
 
       # Extract global variables.
       # @param source [String]
-      def global_variable_extract(source)
+      def global_variable_extract
         raise NotImplementedError
       end
 
--- spec/language/ruby_syntax_spec.rb
+++ spec/language/ruby_syntax_spec.rb
@@ -6,13 +6,55 @@ RSpec.describe Languages::RubySyntax do
     file = File.open("spec/language/ruby_support_test.rb", "rb")
     @rubySyntax = Languages::RubySyntax.new
     @rubySyntax.set_source(file.read)
+    
+    file = File.open("spec/language/rubyBasicSyntaxTest.txt", "rb")
+    @rubySyntax2 = Languages::RubySyntax.new
+    @rubySyntax2.set_source(file.read)
   end
 
   context "#comment_extract" do 
     it "Total of comments" do 
       totalComments = @rubySyntax.comment_extract.size
       expect(@rubySyntax.comment_extract.size).to eq(7)
-    end 
+    end
+
+    it "Comment systax: Correctness" do
+      first = "This file configures a basic test of"
+      first += " ruby syntax. It is no intend to work."
+      comment = @rubySyntax2.comment_extract
+      expect(comment.shift).to eq(first)
+
+      second = " The basic ideia is verify if kuniri extract the necessary "
+      second += "code in the"
+      expect(comment.shift).to eq(second)
+
+      third = "correct way."
+      expect(comment.shift).to eq(third)
+ 
+      fourth = "Test xpto class"
+      expect(comment.shift).to eq(fourth)
+
+      fifth = "something"
+      expect(comment.shift).to eq(fifth)
+
+      sixth = " construct"
+      expect(comment.shift).to eq(sixth)
+
+      seventh = "Code..."
+      expect(comment.shift).to eq(seventh)
+
+      eighth = "... code ..."
+      expect(comment.shift).to eq(eighth)
+
+      nine = "function 3"
+      expect(comment.shift).to eq(nine)
+
+      fourth = "1: Right!"
+      expect(comment.shift).to eq(fourth)
+
+      sixth = "2: Right!"
+      expect(comment.shift).to eq(sixth)
+    end
 
     it "Verify the correctness of the comment extracted" do
       firstCommentString = " RubySupportTest is used only for execute basic" 
@@ -35,6 +77,12 @@ RSpec.describe Languages::RubySyntax do
     it "Class name" do
       classList = @rubySyntax.class_extract
       expect(classList.shift).to eq("RubySupportTest")
+
+      classList2 = @rubySyntax2.class_extract
+      expect(classList2.shift).to eq("Class1GetRight")
+      expect(classList2.shift).to eq("Class2GetRight")
+      expect(classList2.shift).to eq("Class3GetRight")
+      expect(classList2.shift).to eq("Class4MethodTest")
     end
   end
 
@@ -42,6 +90,14 @@ RSpec.describe Languages::RubySyntax do
     it "Method name" do
       methodName = @rubySyntax.method_extract
       expect(methodName.shift).to eq("initialize")
+
+      methodName2 = @rubySyntax.method_extract
+      expect(methodName2.shift.name).to eq("firstMethodRight")
+      methodParameter = methodName2.shift.name
+      expect(methodParameter.name).to eq("secondMethodRight")
+      expect(methodParameter.parameter[0]).to eq("one")
+      expect(methodParameter.parameter[1]).to eq("two")
+      expect(methodParameter.parameter[2]).to eq("three")
     end
   end
 
