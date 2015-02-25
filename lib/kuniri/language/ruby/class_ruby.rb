db620f1eb3ccfa9dd51ef56069ba58062616f2af
Rodrigo <rodrigosiqueiramelo@gmail.com>
2015-02-25 19:48:08 -0300

    Implemented class_ruby test and fix some details.

--- lib/kuniri/language/ruby/class_ruby.rb
+++ lib/kuniri/language/ruby/class_ruby.rb
@@ -1,17 +1,20 @@
 require_relative '../abstract_container/class.rb'
+require_relative '../container_data/class_data.rb'
 
 module Languages
 
   module Ruby
 
     class ClassRuby < Class
-
+    # TODO
       public
 
         def get_class(pLine)
           result = detect_class(pLine)
           return nil unless result
-          return result
+          classCaptured = Languages::ClassData.new
+          classCaptured.name = result
+          return classCaptured
         end
 
       protected
@@ -19,7 +22,7 @@ module Languages
         def detect_class(pLine)
           regexExpression = /^\s*class\b[ |\t]+\s*(.*)\b/
           return nil unless pLine =~ regexExpression
-          return pLine.scan(regexExpression)[0]
+          return pLine.scan(regexExpression)[0].join("")
         end
 
         def get_inharitance
--- spec/language/ruby/class_ruby_spec.rb
+++ spec/language/ruby/class_ruby_spec.rb
@@ -6,64 +6,93 @@ RSpec.describe Languages::Ruby::ClassRuby do
     @classRuby = Languages::Ruby::ClassRuby.new
   end
 
-  context "# Class handling" do
-
-    it "Ordinary class declaration" do
+  context "When is class without inheritance" do
+    it "Ordinary class declaration." do
       classNameCaptured = @classRuby.get_class("class Xpto").name
       expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("     class      Xpto").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("     class      Xpto   ").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("        class Xpto").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("class Xpto        ").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("  class  Xpto      ").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class(" class Xpto  ").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class(" class Xpto").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("class  Xpto  ").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class(" class  Xpto ").name
-      expect(classNameCaptured).to eq("Xpto")
-
-      classNameCaptured = @classRuby.get_class("    class  Xpto  ").name
-      expect(classNameCaptured).to eq("Xpto")
-
     end
 
-    it "Class with inheritance" do
+    it "Class declaration with whitespace." do
+      classNameCaptured = @classRuby.get_class("     class      Xpto").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration between whitespace." do
+      classNameCaptured = @classRuby.get_class("     class      Xpto   ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration with whitespace in the beginning." do
+      classNameCaptured = @classRuby.get_class("        class Xpto").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration with whitespace in the end." do
+      classNameCaptured = @classRuby.get_class("class Xpto        ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration delimited by whitespace." do
+      classNameCaptured = @classRuby.get_class("  class  Xpto      ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration with a few whitespace." do
+      classNameCaptured = @classRuby.get_class(" class Xpto  ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration with whitespace in the beginning." do
+      classNameCaptured = @classRuby.get_class(" class Xpto").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration ended with whitespace." do
+      classNameCaptured = @classRuby.get_class("class  Xpto  ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration with many whitespace." do
+      classNameCaptured = @classRuby.get_class(" class  Xpto ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+
+    it "Class declaration with tab." do
+      classNameCaptured = @classRuby.get_class("    class  Xpto  ").name
+      expect(classNameCaptured).to eq("Xpto")
+    end
+  end
+
+  context "When with inheritance" do
+    it "Simple class inheritance." do
       classNameCaptured = @classRuby.get_class("class Xpto < Abc").name
       expect(classNameCaptured).to eq("Xpto")
+    end
 
+    it "Class inheritance with whitespace in the beginning." do
       classNameCaptured = @classRuby.get_class("     class   Xpto < Abc").name
       expect(classNameCaptured).to eq("Xpto")
+    end
 
+    it "Class inheritance with whitespace between <." do
       classNameCaptured = @classRuby.get_class("class Xpto   < Abc").name
       expect(classNameCaptured).to eq("Xpto")
+    end
 
+    it "Class inheritance with many whitespace." do
       classNameCaptured = @classRuby.get_class("   class   Xpto < Abc ").name
       expect(classNameCaptured).to eq("Xpto")
+    end
 
+    it "Class inheritance with many whitespace between <." do
       classNameCaptured = @classRuby.get_class("class Xpto      <    Abc").name
       expect(classNameCaptured).to eq("Xpto")
+    end
 
+    it "Class inheritance with whitespace in the corners." do
       classNameCaptured = @classRuby.get_class("  class   Xpto < Abc   ").name
       expect(classNameCaptured).to eq("Xpto")
     end
-
   end
 
   after :all do
