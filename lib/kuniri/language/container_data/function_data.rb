a877f232b39f971eccbab011480a1e05c5e9403d
Rodrigo <rodrigosiqueiramelo@gmail.com>
2015-02-25 22:40:05 -0300

    Added function test.

--- lib/kuniri/language/container_data/function_data.rb
+++ lib/kuniri/language/container_data/function_data.rb
@@ -2,6 +2,7 @@ require_relative 'basic_data'
 
 module Languages
 
+  # Handle function data
   class FunctionData < BasicData
 
     public
@@ -16,7 +17,6 @@ module Languages
 
     private
 
-      
 
   # class
   end
--- spec/language/container_data/function_data_spec.rb
+++ spec/language/container_data/function_data_spec.rb
@@ -3,16 +3,16 @@ require_relative '../../spec_helper'
 RSpec.describe Languages::FunctionData do
 
   before :all do
-    @functionData = Languages::FunctionData.new
+    @functionData = Languages::FunctionData.new("globalFunction")
   end
 
-  context "# Function data test" do
-    it "Function name" do
-    end
+  context "When function has no parameter" do
+    it "Set function name" do
+      expect(@functionData.name).to eq("globalFunction")
 
-    it "Function parameters" do
+      @functionData.name = "xpto"
+      expect(@functionData.name).to eq("xpto")
     end
-
   end
 
   after :all do
