3572d469df4bd30f52e0a902c3c427fb0b6f8080
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2015-06-15 23:08:17 -0300

    Sample file for test.

---
+++ spec/samples/rubySyntaxParts/function/simpleFunction.rb
@@ -0,0 +1,29 @@
+def simpleFunction1
+end
+
+def simpleFunction2 (param)
+end
+
+def simpleFunction3 (param)
+
+end
+
+def simpleFunction4 (param)
+
+
+
+
+end
+
+
+def simpleFunction5 (param1, param2)
+  return
+end
+
+def simpleFunction6 (param1)
+  return 0
+end
+
+def simpleFunction7(param1, param2)
+  puts "abcd"
+end
