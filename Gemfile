5574b3065d748baadadf00da7864fca9f2207549
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2015-07-10 17:00:30 -0300

    Updated Gemfile and coveralls.

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gemspec
 group :development do
   gem 'rspec', '~> 3.1.0'
   gem 'guard-rspec', '~> 4.2.10'
-  gem 'simplecov', '~> 0.9.0'
   gem "codeclimate-test-reporter", group: :test, require: nil
   gem 'coveralls', require: false
   gem 'nokogiri', '~> 1.6.6'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,9 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib/kuniri'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'config'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-#require "codeclimate-test-reporter"
-#CodeClimate::TestReporter.start
+
 require 'rspec'
-require 'simplecov'
 require 'coveralls'
-
-if ENV['CI']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-else
-  require 'simplecov'
-  SimpleCov.start
-end
-
-SimpleCov.start
 
 RSpec.configure do |config|
   config.order = 'random'
