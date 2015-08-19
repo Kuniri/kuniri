5a362639d30a4756bfb3febffad23bbb2ae8df64
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-08-19 08:41:40 -0300

    Refactor execute method of repetition_state.rb

--- lib/kuniri/state_machine/OO_structured_fsm/repetition_state.rb
+++ lib/kuniri/state_machine/OO_structured_fsm/repetition_state.rb
@@ -7,14 +7,20 @@ module StateMachine
     # Class responsible for handling Repetition state.
     class RepetitionState < OOStructuredState
 
+      MAP_STATE = {
+        StateMachine::METHOD_STATE => "method",
+        StateMachine::CONSTRUCTOR_STATE => "function",
+        StateMachine::GLOBAL_FUNCTION_STATE => "constructor"
+      }
+
       @language
 
       def initialize(pLanguage)
         @language = pLanguage
       end
 
-        def handle_line(pLine)
-        end
+      def handle_line(pLine)
+      end
 
       # @see OOStructuredState
       def method_capture
@@ -39,33 +45,12 @@ module StateMachine
         repetition = @language.repetitionHandler.get_repetition(pLine)
 
         flag = @language.flagFunctionBehaviour
+        get_add_repetition_lambda(MAP_STATE[flag]).call(repetition, pElementFile)
 
-        if (repetition)
-          #Function
-          if (flag == StateMachine::GLOBAL_FUNCTION_STATE)
-            pElementFile.global_functions.last.add_repetition(repetition)
-          #Method
-          elsif (flag == StateMachine::METHOD_STATE)
-            pElementFile.classes.last.methods.last.add_repetition(repetition)
-          #Constructor
-          elsif (flag == StateMachine::CONSTRUCTOR_STATE)
-            pElementFile.classes.last.constructors
-                        .last.add_repetition(repetition)
-          end
-        end
-
-        if (@language.endBlockHandler.has_end_of_block?(pLine))
-          if (flag == StateMachine::GLOBAL_FUNCTION_STATE)
-            function_capture
-          elsif (flag == StateMachine::METHOD_STATE)
-            method_capture
-          elsif (flag == StateMachine::CONSTRUCTOR_STATE)
-            constructor_capture
-          end
-        end
+        has_end_of_block = @language.endBlockHandler.has_end_of_block?(pLine)
+        get_capture_lambda(MAP_STATE[flag]).call(has_end_of_block)
 
         return pElementFile
-
       end
 
       private
@@ -75,6 +60,30 @@ module StateMachine
           @language.flagFunctionBehaviour = StateMachine::NONE_HANDLING_STATE
         end
 
+        def get_capture_lambda(lambdaType)
+          capture_lambda = lambda do |has_end_of_block|
+            self.send("#{lambdaType}_capture") if has_end_of_block
+          end
+
+          return capture_lambda
+        end
+
+        def get_list_of_file(pElementFile, elementType)
+          if elementType == MAP_STATE[StateMachine::CONSTRUCTOR_STATE]
+            return pElementFile.global_functions
+          else
+            return pElementFile.classes.last.send("#{elementType}s")
+          end
+        end
+
+        def get_add_repetition_lambda(lambdaType)
+          add_repetition_lambda = lambda do |repetition, pElementFile|
+            element = get_list_of_file(pElementFile, lambdaType).last
+            element.add_repetition(repetition) if repetition
+          end
+
+          add_repetition_lambda
+        end
     # End class
     end
 
