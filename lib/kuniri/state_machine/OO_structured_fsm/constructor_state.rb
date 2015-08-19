a96a6243090ceff2304b786900a8abcdb7212281
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-08-18 23:47:29 -0300

    Refactor ConstructorState and MethodState

--- lib/kuniri/state_machine/OO_structured_fsm/constructor_state.rb
+++ lib/kuniri/state_machine/OO_structured_fsm/constructor_state.rb
@@ -1,66 +1,25 @@
 require_relative 'oo_structured_state'
+require_relative 'method_state'
 
 module StateMachine
 
   module OOStructuredFSM
 
     # Class responsible for handling constructor state.
-    class ConstructorState < OOStructuredState
+    class ConstructorState < MethodState
 
-      @language
+      CLASS_TYPE = "constructor"
+      METHOD_TYPE = "constructor"
 
       def initialize(pLanguage)
-        @language = pLanguage
+        super(
+          pLanguage,
+          state: StateMachine::CONSTRUCTOR_STATE,
+          classType: ConstructorState::CLASS_TYPE,
+          methodType: ConstructorState::METHOD_TYPE
+        )
       end
 
-      # @see OOStructuredState
-      def handle_line(pLine)
-        if @language.conditionalHandler.get_conditional(pLine)
-          conditional_capture
-        elsif @language.repetitionHandler.get_repetition(pLine)
-          repetition_capture
-        else
-          return
-        end
-      end
-
-      # @see OOStructuredState
-      def class_capture
-        @language.rewind_state
-      end
-
-      # @see OOStructuredState
-      def conditional_capture
-        @language.flagFunctionBehaviour = StateMachine::CONSTRUCTOR_STATE
-        @language.set_state(@language.conditionalState)
-      end
-
-      # @see OOStructuredState
-      def repetition_capture
-        @language.flagFunctionBehaviour = StateMachine::CONSTRUCTOR_STATE
-        @language.set_state(@language.repetitionState)
-      end
-
-      # @see OOStructuredState
-      def execute(pElementFile, pLine)
-        constructorElement =
-                          @language.constructorHandler.get_constructor(pLine)
-
-        if (constructorElement)
-          lastIndex = pElementFile.classes.length - 1 # We want the index
-          constructorElement.comments = @language.string_comment_to_transfer
-          @language.string_comment_to_transfer = ""
-          pElementFile.classes[lastIndex].add_constructor(constructorElement)
-        end
-
-        if (@language.endBlockHandler.has_end_of_block?(pLine))
-          class_capture
-        end
-
-        return pElementFile
-
-      end
-    
     # End class
     end
 
