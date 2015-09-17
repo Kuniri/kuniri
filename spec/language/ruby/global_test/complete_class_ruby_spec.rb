53942367186653779c12e2860f7670157dbc3787
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2015-09-17 20:12:25 -0300

    Now Kuniri runs in a single file
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
    Signed-off-by: Filipe Borges <filipe_bkc@yahoo.com.br>
    Signed-off-by: Thabata Granja <thabata.helen@gmail.com>
    Signed-off-by: Simião Carvalho <simiaosimis@gmail.com>
    Signed-off-by: JohnnysRibeiro <johnnys.ribeiro@gmail.com>

--- spec/language/ruby/global_test/complete_class_ruby_spec.rb
+++ spec/language/ruby/global_test/complete_class_ruby_spec.rb
@@ -3,18 +3,20 @@ require_relative '../../../../lib/kuniri/core/kuniri'
 RSpec.describe Kuniri::Kuniri do
 
   before :each do
-    @path = "/spec/language/ruby/global_test/.kuniri"  
+    @path = "/spec/language/ruby/global_test/kuniri-config"
     @kuniri = Kuniri::Kuniri.new(@path)
   end
 
   it "Correct structure of class - Class" do
-    pLanguage ="**.rb"
-    @filesProject = Dir[File.join(@path, "**", pLanguage)]
+    #pLanguage ="**.rb"
+    #@filesProject = Dir[File.join(@path, "**", pLanguage)]
 
-    @parser = Parser::Parser.new(@filesProject)
-    @parser.start_parser()
+    #@parser = Parser::Parser.new(@filesProject)
+    #@parser.start_parser()
+    @kuniri.run_analysis
 
     parser = Parser::XML.new
+    parser.set_path("./spec/language/ruby/global_test/output_test.xml")
     parser.create_all_data @kuniri.get_parser()
   end
 
