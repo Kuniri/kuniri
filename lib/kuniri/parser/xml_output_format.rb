8408cbff052d2852923a768af4a408bb07428803
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2015-09-20 19:17:09 -0300

    Fix xml output

--- lib/kuniri/parser/xml_output_format.rb
+++ lib/kuniri/parser/xml_output_format.rb
@@ -21,16 +21,16 @@ module Parser
         # Go through elements
         # 1 - Inheritances
         temporaryStringXML = inheritance_generate(pClass.inheritances)
-        buildClassString += temporaryStringXML if temporaryStringXML
+        buildClassString += temporaryStringXML unless temporaryStringXML
         # 2 - Attributes
         temporaryStringXML = attribute_generate(pClass.attributes)
-        buildClassString += temporaryStringXML if temporaryStringXML
+        buildClassString += temporaryStringXML unless temporaryStringXML
         # 3 - Constructors
         temporaryStringXML = constructor_generate(pClass.constructors)
-        buildClassString += temporaryStringXML if temporaryStringXML
+        buildClassString += temporaryStringXML unless temporaryStringXML
         # 4 - Methods
         temporaryStringXML = method_generate(pClass.methods)
-        buildClassString += temporaryStringXML if temporaryStringXML
+        buildClassString += temporaryStringXML unless temporaryStringXML
         # 5 - TODO: You have to handler class inside class
 
         # Close class
@@ -41,7 +41,9 @@ module Parser
 
       # @see OutputFormat
       def constructor_generate(pConstructor)
-        buildConstructorStringXML = ""
+
+        return "" if pConstructor.empty?
+
         # Special case, empty constructor
         if (pConstructor.parameters.empty? &&
             pConstructor.conditionals.empty? &&
@@ -49,17 +51,18 @@ module Parser
             return default_fields("constructor", pConstructor)
         end
 
+        buildConstructorStringXML = ""
         buildConstructorStringXML += default_fields("constructor",
                                                     pConstructor, false)
 
         # 1 - Handle parameters
         temporaryStringXML = parameters_generate(pConstructor)
-        buildConstructorStringXML += temporaryStringXML if temporaryStringXML
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
         # 2 - Conditional
         temporaryStringXML = conditional_generate(pConstructor)
-        buildConstructorStringXML += temporaryStringXML if temporaryStringXML
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
         # 3 - Repetition
-        buildConstructorStringXML += temporaryStringXML if temporaryStringXML
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
         # 4 - TODO: local variable
 
         buildConstructorStringXML += "\n</constructor>"
@@ -69,31 +72,38 @@ module Parser
 
       # @see OutputFormat
       def inheritance_generate(pInheritances)
+
+        return "" if pInheritances.empty?
+
         inheritanceString = []
         pInheritances.each do |inheritance|
           inheritanceString.push("<inheritance name=\"#{inheritance}\" />")
         end
+
         return inheritanceString.join("\n")
       end
 
       # @see OutputFormat
       def method_generate(pMethod)
+
+        return "" if pMethod.empty?
+
         # Empty method
         if (pMethod.parameters.empty? && pMethod.conditionals.empty? &&
             pMethod.repetitions.empty?)
             return default_fields("method", pMethod)
         end
 
-         default_fields("method", pMethod, false)
+        default_fields("method", pMethod, false)
 
         # 1 - Handle parameters
         temporaryStringXML = parameters_generate(pConstructor.parameters)
-        buildConstructorStringXML += temporaryStringXML if temporaryStringXML
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
         # 2 - Conditional
         temporaryStringXML = conditional_generate(pConstructor)
-        buildConstructorStringXML += temporaryStringXML if temporaryStringXML
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
         # 3 - Repetition
-        buildConstructorStringXML += temporaryStringXML if temporaryStringXML
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
         # 4 - TODO: local variable
 
         buildConstructorStringXML += "\n</constructor>"
@@ -102,19 +112,27 @@ module Parser
 
       # @see OutputFormat
       def parameters_generate(pParameters)
+
+        return "" if pParameters.empty?
+
         buildParameterString = []
         pParameters.each do |parameter|
            buildParameterString.push("<parameter name=\"#{parameter}\">")
         end
+
         return buildParameterString.join("\n")
       end
 
       # @see OutputFormat
       def attribute_generate(pAttribute)
+
+        return "" if pAttribute.empty?
+
         buildParameterString = []
-        pAttribute.each do |attribute|    
+        pAttribute.each do |attribute|
           buildAttributeString.push("<attribute name=\"#{attribute}\">")
         end
+
         return buildAttributeString.join("\n")
       end
 
@@ -126,37 +144,53 @@ module Parser
 
       # @see OutputFormat
       def global_variable_generate(pGlobalVariable)
+
+        return "" if pGlobalVariable.empty?
+
         buildGlobalVariableString = []
+
         pGlobalVariable.each do |globalVariable|
           buildGlobalVariableString.push("<globalVariable " +
-                                          "name=\"#{globalVariable}\" />")
+                                         "name=\"#{globalVariable}\" />")
         end
+
         return buildGlobalVariableString.join("\n")
       end
 
       # @see OutputFormat
       def extern_requirement_generate(pRequire)
         # TODO: Extern requirement not follows the global behaviour. Fix it.
+
+        return "" if pRequire.empty?
+
         buildExternRequirement = []
         pRequire.each do |path|
           buildExternRequirement.push("<require name=\"#{path}\" />")
         end
+
         return buildExternRequirement.join("\n")
       end
 
       # @see OutputFormat
       def repetition_generate(pRepetition)
+
+        return "" if pRepetition.empty?
+
         buildRepetition = []
         # TODO: We have to call it recursively and inspect for conditional too.
         pRepetition.each do |repetition|
           buildRepetition.push("<repetition type=\"#{repetition.type}\" " +
                                "expression=\"#{repetition.expression}\" />")
         end
+
         return buildRepetition.join("\n")
       end
 
       # @see OutputFormat
       def module_generate(pModule)
+
+        return "" if pModule.empty?
+
         buildModule = []
         pModule.each do |modules|
           buildModule.push(default_fields("module", pModule))
@@ -167,11 +201,15 @@ module Parser
 
       # @see OutputFormat
       def conditional_generate(pConditional)
+
+        return "" if pConditional.empty?
+
         buildConditional = []
         buildConditional.each do |conditional|
           buildConditional.push("<conditional type=\"#{conditional}\" " +
                                 "expression=\"#{conditional.expression}\" />")
         end
+
         return buildConditional.join("\n")
       end
 
@@ -184,6 +222,34 @@ module Parser
                         "\n\tcomments=\"#{pElement.comments}\" "
         buildString += (pCloseIt) ? "/>" : ">"
         return buildString
+      end
+
+      def handle_function_behavior
+
+        return "" if pMethod.empty?
+
+        # Empty method
+        if (pMethod.parameters.empty? && pMethod.conditionals.empty? &&
+            pMethod.repetitions.empty?)
+            return default_fields("method", pMethod)
+        end
+
+        default_fields("method", pMethod, false)
+
+        #send(#{begin}_generate(element))
+        # 1 - Handle parameters
+        temporaryStringXML = parameters_generate(pConstructor.parameters)
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
+        # 2 - Conditional
+        temporaryStringXML = conditional_generate(pConstructor)
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
+        # 3 - Repetition
+        buildConstructorStringXML += temporaryStringXML unless temporaryStringXML
+        # 4 - TODO: local variable
+
+        buildConstructorStringXML += "\n</constructor>"
+        return buildConstructorStringXML
+
       end
 
   # class
