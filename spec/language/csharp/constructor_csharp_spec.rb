0d6bbe5830f3323d616f73cb4288059802bb90f3
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2015-10-15 18:53:37 -0300

    Handling constructors

---
+++ spec/language/csharp/constructor_csharp_spec.rb
@@ -0,0 +1,57 @@
+require_relative '../../spec_helper'
+
+RSpec.describe Languages::Csharp::ConstructorCsharp do
+
+  before :all do
+    @constructor = Languages::Csharp::ConstructorCsharp.new
+  end
+
+  context "When constructor without parameter" do
+    it "Simple declaration" do
+      input = "public Xpto()"
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+    end
+
+    it "With spaces in the begin" do
+      input = "         public Xpto()"
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+    end
+
+    it "With spaces in the begin and between the constructor name" do
+      input = "        public     Xpto()"
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+      input = "                 public              Xpto()"
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+    end
+
+    it "With spaces in the end" do
+      input = "public Xpto()                   "
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+      input = "public                    Xpto()                   "
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+    end
+
+    it "With spaces in both sides" do
+      input = "           public              Xpto()              "
+      expect(@constructor.get_constructor(input).name)
+             .to eq("Xpto")
+    end
+
+    it "No constructor" do
+      input = "public int methodX()"
+      expect(@constructor.get_constructor(input))
+             .to eq(nil) 
+    end
+  end
+
+  after :all do
+    @constructor = nil
+  end
+
+end 
