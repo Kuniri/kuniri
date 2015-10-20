226e20638bbe270c66a51d8428bb0d0e7aaa0a39
Filipe Borges <filipe_bkc@yahoo.com.br>
2015-10-20 18:37:12 -0200

    Fixing class parser error

--- lib/kuniri/language/java/class_java.rb
+++ lib/kuniri/language/java/class_java.rb
@@ -39,30 +39,29 @@ module Languages
 
         def detect_class(pLine)
           pLine.gsub!(/\s{2,}/, " ")
-          access_regex =  /(?:(?:public|private|protected)\s)/
-          modifiers_regex = /(?:(?:abstract|final|static)\s)/
-          inheritance_regex = /(?:\sextends\s(?:\w+))/
-          regexExpression = /\s?#{access_regex}?#{modifiers_regex}?class\s(\w+)#{inheritance_regex}?/
+          access =  /(?:(?:public|private|protected)\s)/
+          modifiers = /(?:(?:abstract|final|static)\s)/
+          regexExpression = /\s?#{access}?#{modifiers}?class\s(.+)/
           return nil unless pLine =~ regexExpression
           return pLine.scan(regexExpression)[0].join("")
         end
 
         def get_inheritance(pString)
-          if pString =~ /</
-            partial = pString.scan(/<(.*)/)
+          if pString =~ /extends/
+            partial = pString.scan(/extends\s(\w+)/)
             return remove_unnecessary_information(partial)
           end
           return nil
         end
 
       def remove_unnecessary_information(pString)
-        return pString.gsub(/\s|</, "") if pString =~ /\s|</
+        return pString.gsub(/\s|\{/, "") if pString =~ /\s|\{/
         return pString
       end
 
       def prepare_final_string(pString)
-        if pString =~ /\s|</
-          partial = pString.gsub(/<.*/,"")
+        if pString =~ /\s|extends|\{/
+          partial = pString.gsub(/extends.*/,"")
           return remove_unnecessary_information(partial)
         end
         return pString
