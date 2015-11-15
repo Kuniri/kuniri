6bbd48cca34dd6fd7e6c4bac46241f5c465a6b38
Lucas Moura <lucas.moura128@gmail.com>
2015-11-15 12:11:40 -0200

    Add aggregation state file
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

---
+++ lib/kuniri/state_machine/OO_structured_fsm/aggregation_state.rb
@@ -0,0 +1,44 @@
+require_relative 'oo_structured_state'
+
+module StateMachine
+
+  module OOStructuredFSM
+
+    # Class responsible for handling aggregation state.
+    class AggregationState < OOStructuredState
+
+      @language
+
+      def initialize(pLanguage)
+        @language = pLanguage
+      end
+
+      # @see OOStructuredState
+      def handle_line(pLine)
+      end
+
+      # @see OOStructuredState
+      def class_capture
+        @language.rewind_state
+      end
+
+      # @see OOStructuredState
+      def execute(pElementFile, pLine)
+        aggregationElement = @language.aggregationHandler.get_aggregation(pLine)
+
+        if aggregationElement
+            pElementFile.classes.last.aggregation.push aggregationElement
+        end
+
+        class_capture
+        return pElementFile
+      end
+
+    # End class
+    end
+
+  # End OOStructuredFSM
+  end
+
+# End StateMachine
+end
