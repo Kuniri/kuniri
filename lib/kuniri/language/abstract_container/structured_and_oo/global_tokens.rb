e2a573a072aac23ac1ff6b6c114f4983652e81cc
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2015-12-26 23:16:36 -0200

    Add manager element

---
+++ lib/kuniri/language/abstract_container/structured_and_oo/global_tokens.rb
@@ -0,0 +1,14 @@
+module Languages
+
+  # Token types used for standardize assignments.
+  IF_LABEL ||= "IF"
+  ELSE_LABEL ||= "ELSE"
+  ELSIF_LABEL ||= "ELSIF"
+  CASE_LABEL ||= "CASE"
+  WHILE_LABEL ||= "WHILE"
+  FOR_LABEL ||= "FOR"
+  DO_WHILE_LABEL ||= "DOWHILE"
+  UNTIL_LABEL ||= "UNTIL"
+  LAMBDA_LABEL ||= "LAMBDA"
+
+end
---
+++ lib/kuniri/language/container_data/structured_and_oo/manager_basic_structure_data.rb
@@ -0,0 +1,40 @@
+require_relative 'basic_data'
+
+module Languages
+
+  # ManagerBasicStructureData Class responsible for management conditionals
+  # and repetition.
+  class ManagerBasicStructureData < Languages::BasicData
+
+    public
+
+      attr_accessor :basicStructure
+
+      # Add conditional to basicStructure
+      # @param pConditional ConditionalData to add inside basicStructure
+      def add_conditional(pConditional)
+        # TODO
+      end
+
+      # Add repetition to basicStructure
+      # @param pRepetition RepetitionData to add inside basicStrure
+      def add_repetition(pRepetition)
+        # TODO
+      end
+
+      # Add nested level, basically, if we have a lot of nested conditional or
+      # repetition we add one level for each nest.
+      def add_level
+        # TODO
+      end
+
+      # Up current level
+      def up_level
+        # TODO
+      end
+
+  # End class
+  end
+
+# End module
+end
