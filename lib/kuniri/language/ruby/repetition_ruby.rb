8673893f08df1e84095b10c974c962a699e7d2aa
Rodrigo Siqueira <siqueira@kuniri.org>
2016-01-20 20:33:58 -0200

    Replace strings by constants

--- lib/kuniri/language/ruby/repetition_ruby.rb
+++ lib/kuniri/language/ruby/repetition_ruby.rb
@@ -55,19 +55,19 @@ module Languages
         # Override
         def repetition_type(pString)
           regexExp = /^\s+while|^while/
-          return "WHILE" if regexExp =~ pString
+          return Languages::WHILE_LABEL if regexExp =~ pString
 
           regexExp = /^\s+for|^for/
-          return "FOR" if regexExp =~ pString
+          return Languages::FOR_LABEL if regexExp =~ pString
 
           regexExp = /^\s+until|^until/
-          return "UNTIL" if regexExp =~ pString
+          return Languages::UNTIL_LABEL if regexExp =~ pString
 
           regexExp = /^\s*end\s+while\s+/
-          return "DOWHILE" if regexExp =~ pString
+          return Languages::DO_WHILE_LABEL if regexExp =~ pString
 
           regexExp = /^lambda\s+/
-          return "LAMBDA" if regexExp =~ pString
+          return Languages::LAMBDA_LABEL if regexExp =~ pString
 
           regexExp = /\.\w+/
           return pString[/\w+/, 0].upcase if regexExp =~ pString
@@ -77,19 +77,20 @@ module Languages
 
         # Override
         def get_expression(pType, pString)
-          if pType == "FOR"
-            pString.slice!("for")
-          elsif pType == "WHILE"
-            pString.slice!("while")
-            pString.slice!("do")
-          elsif pType == "UNTIL"
-            pString.slice!("until")
-            pString.slice!("do")
-          elsif pType == "DOWHILE"
-            pString.slice!("end")
-            pString.slice!("while")
-          else
-            pString.slice!(".")
+          case pType
+            when Languages::FOR_LABEL
+              pString.slice!("for")
+            when Languages::WHILE_LABEL
+              pString.slice!("while")
+              pString.slice!("do")
+            when Languages::UNTIL_LABEL
+              pString.slice!("until")
+              pString.slice!("do")
+            when Languages::DO_WHILE_LABEL
+              pString.slice!("end")
+              pString.slice!("while")
+            else
+              pString.slice!(".")
           end
           leftStrip = pString.lstrip
           rightStrip = leftStrip.rstrip
