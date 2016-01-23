eb90c5ad4da25f73a76f12fa7925787e04c70495
Rodrigo Siqueira <siqueira@kuniri.org>
2016-01-22 23:10:28 -0200

    Fixed: CoditionalData and RepetitionData was igual

--- lib/kuniri/language/container_data/structured_and_oo/conditional_data.rb
+++ lib/kuniri/language/container_data/structured_and_oo/conditional_data.rb
@@ -1,21 +1,9 @@
-require_relative 'basic_data'
+require_relative 'basic_structure'
 
 module Languages
 
   # Class responsible for handling conditional data
-  class ConditionalData < Languages::BasicData
-
-    public
-
-      attr_accessor :type
-      attr_accessor :expression
-      attr_accessor :level
-
-      def initialize
-        @type = "none"
-        @expression = "empty"
-        @level = 0
-      end
+  class ConditionalData < Languages::BasicStructure
 
   # class
   end
