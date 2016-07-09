8d131d50b89c4c490326ee252b2af52fe992f944
Siqueira <rodrigosiqueiramelo@gmail.com>
2016-07-09 09:54:34 -0300

    Changed the way to add inheritance to class

--- spec/language/container_data/structured_and_oo/class_data_spec.rb
+++ spec/language/container_data/structured_and_oo/class_data_spec.rb
@@ -4,29 +4,70 @@ RSpec.describe Languages::ClassData do
 
   before :each do
     @classData = Languages::ClassData.new
-    @classData.name = "Xpto"
+    @classData.name = 'Xpto'
   end
 
-  context "When set and get class name" do
+  context 'When set and get class name' do
 
-    it "Get class name." do
-      expect(@classData.name).to eq("Xpto")
+    it 'Get class name.' do
+      expect(@classData.name).to eq('Xpto')
     end
 
-    it "Set class name." do
-      @classData.name = "Abc"
-      expect(@classData.name).to eq("Abc")
+    it 'Set class name.' do
+      @classData.name = 'Abc'
+      expect(@classData.name).to eq('Abc')
     end
 
-    it "Visibility." do
-      expect(@classData.visibility).to eq("public")
+    it 'Visibility.' do
+      expect(@classData.visibility).to eq('public')
 
-      @classData.visibility = "private"
-      expect(@classData.visibility).to eq("private")
+      @classData.visibility = 'private'
+      expect(@classData.visibility).to eq('private')
     end
   end
 
-  RSpec.shared_examples "Add attribute" do |attributes, description|
+  context 'Add attribute correctly' do
+    # TODO
+  end
+
+  context 'Add method correctly' do
+    # TODO
+  end
+
+  context 'Add constructor correctly' do
+    # TODO
+  end
+
+  context 'Add inheritance correctly' do
+    it 'Simple case of add inheritance' do
+      expect(@classData.inheritances).to eq([])
+
+      @classData.add_inheritance('Kuniri')
+      expect(@classData.inheritances).to match_array(['Kuniri'])
+      @classData.add_inheritance('Now')
+      expect(@classData.inheritances).to match_array(['Kuniri', 'Now'])
+    end
+
+    it 'Should not accept repetition' do
+      expect(@classData.inheritances).to eq([])
+
+      @classData.add_inheritance('Kuniri')
+      expect(@classData.inheritances).to match_array(['Kuniri'])
+      @classData.add_inheritance('Kuniri')
+      expect(@classData.inheritances).to match_array(['Kuniri'])
+      @classData.add_inheritance('Now')
+      expect(@classData.inheritances).to match_array(['Kuniri', 'Now'])
+    end
+
+    it 'Wrong type of inheritance' do
+      expect(@classData.inheritances).to eq([])
+
+      @classData.add_inheritance(['NotAllowed'])
+      expect(@classData.inheritances).to eq([])
+    end
+  end
+
+  RSpec.shared_examples 'Add attribute' do |attributes, description|
 
     it ": #{description}" do
 
@@ -50,7 +91,7 @@ RSpec.describe Languages::ClassData do
 
   end
 
-  RSpec.shared_examples "Add method" do |methods, description|
+  RSpec.shared_examples 'Add method' do |methods, description|
 
     it ": #{description}" do
 
