4c5674295215c3fbe8fd4c6843db0a992ce2515b
Siqueira <rodrigosiqueiramelo@gmail.com>
2016-07-10 08:57:50 -0300

    Added tests to ClassData
    Closes #168

--- spec/language/container_data/structured_and_oo/class_data_spec.rb
+++ spec/language/container_data/structured_and_oo/class_data_spec.rb
@@ -27,15 +27,62 @@ RSpec.describe Languages::ClassData do
   end
 
   context 'Add attribute correctly' do
-    # TODO
+    it 'Normal behaviour to add attribute' do
+      attrs = ['attr1', 'attr2']
+      expect(@classData.attributes).to eq([])
+      attr1 = Languages::AttributeData.new(attrs[0])
+      attr2 = Languages::AttributeData.new(attrs[1])
+      @classData.add_attribute([attr1])
+      expect(@classData.attributes).to match_array([attr1])
+      @classData.add_attribute([attr2])
+      expect(@classData.attributes).to match_array([attr1, attr2])
+    end
+
+    it 'Try to add something different of AttributeData' do
+      expect(@classData.attributes).to eq([])
+      fake_attr = Languages::MethodData.new('abc')
+      @classData.add_attribute([fake_attr])
+      expect(@classData.attributes).to eq([])
+      @classData.add_attribute([1, 3, 4, 5])
+      expect(@classData.attributes).to eq([])
+    end
   end
 
   context 'Add method correctly' do
-    # TODO
+    it 'Normal behaviour when try to add methods inside class' do
+      expect(@classData.methods).to eq([])
+      method1 = Languages::MethodData.new('m1')
+      @classData.add_method(method1)
+      expect(@classData.methods).to match_array([method1])
+      method2 = Languages::MethodData.new('m2')
+      @classData.add_method(method2)
+      expect(@classData.methods).to match_array([method1, method2])
+    end
+
+    it 'Try to add something different of MethodData' do
+      expect(@classData.methods).to eq([])
+      fake_method = Languages::AttributeData.new('abc')
+      @classData.add_method(fake_method)
+      expect(@classData.methods).to eq([])
+    end
   end
 
   context 'Add constructor correctly' do
-    # TODO
+    it 'Normal behaviour when add constructors' do
+      expect(@classData.constructors).to eq([])
+      const1 = Languages::ConstructorData.new('abc')
+      @classData.add_constructor(const1)
+      expect(@classData.constructors).to match_array([const1])
+      const2 = Languages::ConstructorData.new('abc2')
+      @classData.add_constructor(const2)
+      expect(@classData.constructors).to match_array([const1, const2])
+    end
+
+    it 'Try to add something different of ConstructorData' do
+      fake_const = Languages::MethodData.new('abc')
+      @classData.add_constructor(fake_const)
+      expect(@classData.constructors).to match_array([])
+    end
   end
 
   context 'Add inheritance correctly' do
