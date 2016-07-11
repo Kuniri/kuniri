c61c112f43584c624de4e6affa0aa527a7cee5d7
Siqueira <rodrigosiqueiramelo@gmail.com>
2016-07-11 06:57:30 -0300

    Conditional and repetition problems samples

---
+++ spec/samples/rubySyntaxParts/conditionalStatment/conditionalAlone.rb
@@ -0,0 +1,23 @@
+if $0 == 'xpto'
+   puts 'yes...'
+end
+
+if true
+  puts 'seriously?'
+elsif 'maybe' == "maybe"
+  puts 'ahhhh'
+else
+  puts 'hummmmm'
+end
+
+if true
+  if true
+    if true
+      puts 'meeee'
+    elsif true
+      puts 'noooo'
+    else
+      puts 'wtf?'
+    end
+  end
+end
---
+++ spec/samples/rubySyntaxParts/fullCode/conditionalAndRepetitionAlone.rb
@@ -0,0 +1,11 @@
+if $0 == 'xpto'
+   puts 'yes...'
+end
+
+$i = 0
+$num = 5
+
+while $i < $num  do
+   puts("Inside the loop i = #$i" )
+   $i +=1
+end
