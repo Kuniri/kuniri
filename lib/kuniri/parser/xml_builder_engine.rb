df8f3a81555653cb3af92629fa1dcaf66b2b1924
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-01-29 11:19:31 -0200

    Refactored code to make it less complex
    Current code is really complex, mainly on method_missing method. Hence, I broke
    it in parts. Additionally, I did some basic refactors related with code style

--- lib/kuniri/parser/xml_builder_engine.rb
+++ lib/kuniri/parser/xml_builder_engine.rb
@@ -14,95 +14,96 @@ module Parser
     attr_accessor :identIncrement
     attr_reader :optimizationLevel
 
-    public
+    def initialize(pBaseSpaceIdent = 2, pLevel = 0)
+      set_default(pBaseSpaceIdent)
+      @optimizationLevel = pLevel
+    end
 
-      def initialize(pBaseSpaceIdent = 2, pLevel = 0)
-        set_default(pBaseSpaceIdent)
-        @optimizationLevel = pLevel
-      end
+    def to_xml
+      return @tag
+    end
 
-      def to_xml
-        return @tag
-      end
+    def reset_engine
+      @tag = nil
+      set_default
+    end
 
-      def reset_engine
-        @tag = nil
-        set_default
-      end
-
-      def set_default(pBaseSpaceIdent = 2)
-        @tag = '<?xml version="1.0" encoding="UTF-8"?>'
-        @baseSpaceIdentation = pBaseSpaceIdent
-        @content = ''
-        @ident = ''
-        @identIncrement = ' ' * pBaseSpaceIdent
-      end
+    def set_default(pBaseSpaceIdent = 2)
+      @tag = '<?xml version="1.0" encoding="UTF-8"?>'
+      @baseSpaceIdentation = pBaseSpaceIdent
+      @content = ''
+      @ident = ''
+      @identIncrement = ' ' * pBaseSpaceIdent
+    end
 
     private
 
-      def method_missing(pTagName, *pTagParameters, &pBlock)
-        @tag += "\n" unless @tag.strip == ''
-        name = @optimizationLevel != 1 ? pTagName.to_s
-                                       : optimizeSize(pTagName)
-        currentTagName = "#{@ident}<#{name}"
-        @tag << currentTagName
-        if pTagParameters.size > 0 and pTagParameters[0].is_a? Hash
-          handleArguments(pTagParameters)
-        end
+    def method_missing(pTagName, *pTagParameters, &pBlock)
+      @tag += "\n" unless @tag.strip == ''
+      name = @optimizationLevel != 1 ? pTagName.to_s : optimize_size(pTagName)
+      currentTagName = "#{@ident}<#{name}"
+      @tag << currentTagName
 
-        if block_given?
-          @tag << ">\n"
-          incrementIdentation
-          result = instance_eval(&pBlock)
-          if result != '' && result.is_a?(String)
-            updateTag(result)
-          end
-
-          decrementIdent
-          @tag << "#{@ident}</#{name}>\n"
-          @tag.squeeze!("\n")
-        else
-          @tag << "/>\n"
-        end
-          @content = ''
+      if ((!pTagParameters.empty?) && (pTagParameters[0].is_a? Hash))
+        handle_arguments(pTagParameters)
       end
 
-      def optimizeSize(pTagName)
-          return OPTIMIZE_OUTPUT_MAP[pTagName.to_sym] || pTagName.to_s
+      if (block_given?)
+        open_block_tag(&pBlock)
+        close_block_tag(name)
+      else
+        @tag << "/>\n"
       end
 
-      def updateTag(pNewContent)
-        @content = "\n#{@ident}" + pNewContent + "\n"
-        @tag << @content
-      end
+      @content = ''
+    end
 
-      def handleArguments(pTagParameters)
-        pTagParameters[0].each do |key, value|
-          stringValue = handleSpecialCharactersXml(value.to_s)
-          @tag << " #{key.to_s}=\"#{stringValue}\""
-        end
-      end
+    def optimize_size(pTagName)
+      return OPTIMIZE_OUTPUT_MAP[pTagName.to_sym] || pTagName.to_s
+    end
 
-      def handleSpecialCharactersXml(pString)
-        if pString.count('"') > 0 || pString.count("&") > 0 ||
-            pString.count('>') > 0 || pString.count('<') > 0
-            pString = pString.gsub('"', "'")
-                              .gsub('&', '&amp;')
-                              .gsub('>', '&gt;')
-                              .gsub('<', '&lt;')
-          end
-        return pString
-      end
+    def open_block_tag(&pBlock)
+      @tag << ">\n"
+      increment_identation
+      result = instance_eval(&pBlock)
+      update_tag(result) if result != '' && result.is_a?(String)
+    end
 
-      def incrementIdentation
-        @ident += @identIncrement
-      end
+    def close_block_tag(pName)
+      decrement_ident
+      @tag << "#{@ident}</#{pName}>\n"
+      @tag.squeeze!("\n")
+    end
 
-      def decrementIdent
-        if @ident.size > 0
-          @ident = @ident[0...(@ident.size - @baseSpaceIdentation)]
-        end
+    def update_tag(pNewContent)
+      @content = "\n#{@ident}" + pNewContent + "\n"
+      @tag << @content
+    end
+
+    def handle_arguments(pTagParameters)
+      pTagParameters[0].each do |key, value|
+        stringValue = handle_special_characters_xml(value.to_s)
+        @tag << " #{key}=\"#{stringValue}\""
       end
+    end
+
+    def handle_special_characters_xml(pString)
+      if (pString.count('"').positive? || pString.count('&').positive? ||
+          pString.count('>').positive? || pString.count('<').positive?)
+        pString = pString.tr('"', "'").gsub('&', '&amp;').gsub('>', '&gt;')
+                         .gsub('<', '&lt;')
+      end
+      return pString
+    end
+
+    def increment_identation
+      @ident += @identIncrement
+    end
+
+    def decrement_ident
+      return if @ident.empty?
+      @ident = @ident[0...(@ident.size - @baseSpaceIdentation)]
+    end
 
   end
 end
