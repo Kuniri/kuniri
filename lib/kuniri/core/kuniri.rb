43ca4a02af8c05644b69a71d0daec5bd50d99e3a
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-02-07 15:10:12 -0200

    Added Logger example to verify how to use it

--- lib/kuniri/core/kuniri.rb
+++ lib/kuniri/core/kuniri.rb
@@ -6,6 +6,7 @@
 
 require_relative '../error/configuration_file_error'
 require_relative '../parser/parser'
+require_relative '../util/logger_kuniri.rb'
 require_relative 'configuration/language_available'
 require_relative 'setting'
 
@@ -16,77 +17,76 @@ module Kuniri
   # class launch the application.
   class Kuniri
 
-    public
+    attr_reader :configurationInfo # !@attribute Hash with configuration
 
-      attr_reader :configurationInfo # !@attribute Hash with configuration description
+    # @param pPath Receives the path of configuration file. If any element
+    #         is given, it tries to find in the current folder.
+    def initialize
+      @configurationInfo = {}
+      @filesPathProject = []
+      @parserFiles = []
+      @parser = nil
+    end
 
-      # @param pPath Receives the path of configuration file. If any element
-      #         is given, it tries to find in the current folder.
-      def initialize
-        @configurationInfo = {}
-        @filesPathProject = []
-        @parserFiles = []
-        @parser = nil
+    def read_configuration_file(pPath = '.kuniri.yml')
+      @settings = Setting.create
+      @settings.initializate_settings
+      @settings.read_configuration_file(pPath)
+      @configurationInfo = @settings.configurationInfo
+    end
 
+    def set_configuration(pSource, pLanguage, pOutput, pLevel)
+      @settings = Setting.create
+      @settings.initializate_settings
+      @configurationInfo = {language: pLanguage,
+                            source: pSource,
+                            output: pOutput,
+                            pLevel: pLevel}
+
+      @settings.set_configuration(@configurationInfo)
+    end
+
+    # Start Kuniri tasks based on configuration file. After read
+    # configuration file, find all files in source directory.
+    def run_analysis
+      @filesPathProject = get_project_file(@configurationInfo[:source])
+      unless @filesPathProject
+        message = "Problem on source path: #{@configurationInfo[:source]}"
+        Util::LoggerKuniri.error(message)
+        return -1
       end
+      @parser = Parser::Parser.new(@filesPathProject,
+                                   @configurationInfo[:language])
+      Util::LoggerKuniri.info('Start parse...')
+      @parser.start_parser
+    end
 
-      def read_configuration_file(pPath = ".kuniri.yml")
-        @settings = Setting.create
-        @settings.initializate_settings
-        @settings.read_configuration_file(pPath)
-        @configurationInfo = @settings.configurationInfo
-      end
-
-      def set_configuration(pSource, pLanguage, pOutput, pLevel)
-        @settings = Setting.create
-        @settings.initializate_settings
-        @configurationInfo =  {:language => pLanguage,
-                               :source => pSource,
-                               :output => pOutput,
-                               :pLevel => pLevel}
-
-        @settings.set_configuration(@configurationInfo)
-      end
-
-      # Start Kuniri tasks based on configuration file. After read
-      # configuration file, find all files in source directory.
-      def run_analysis()
-        @filesPathProject = get_project_file(@configurationInfo[:source])
-        unless @filesPathProject
-          puts "Problem on source path: #{@configurationInfo[:source]}"
-          return -1
-        end
-        @parser = Parser::Parser.new(@filesPathProject,
-                                      @configurationInfo[:language])
-  	    @parser.start_parser()
-      end
-
-      def get_parser
-        @parser
-      end
+    def get_parser
+      @parser
+    end
 
     private
 
-      @filesProject      # !@attribute Array with object reference of all files
-      @parser            # !@attribute Execute the parser based on settings.
-      @parserFiles       # !@attribute Final output from parser.
+    @filesProject      # !@attribute Array with object reference of all files
+    @parser            # !@attribute Execute the parser based on settings.
+    @parserFiles       # !@attribute Final output from parser.
 
-      # !@param pPath Relative path of the project.
-      # !@param pLanguage Language extension for make the parser.
-      def get_project_file(pPath="./", pLanguage="**.rb")
-        # Verify if path is a valid directory or file
-        return nil unless File.file?(pPath) or File.directory?(pPath)
+    # !@param pPath Relative path of the project.
+    # !@param pLanguage Language extension for make the parser.
+    def get_project_file(pPath = './', pLanguage = '**.rb')
+      # Verify if path is a valid directory or file
+      return nil unless File.file?(pPath) or File.directory?(pPath)
 
-        # Handle single file
-        if (File.file?(pPath))
-          @filesProject = [pPath]
-        # Handle multiple files
-        else
-          @filesProject = Dir[File.join(pPath, "**", pLanguage)]
-        end
-
-        return @filesProject
+      # Handle single file
+      if (File.file?(pPath))
+        @filesProject = [pPath]
+      # Handle multiple files
+      else
+        @filesProject = Dir[File.join(pPath, '**', pLanguage)]
       end
+
+      return @filesProject
+    end
 
   # Class
   end
