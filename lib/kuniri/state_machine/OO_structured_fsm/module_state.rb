831655f20e5cd70a078761f761e088bdff2a0dac
Bruno <bruno.sesso@usp.br>
2017-04-05 13:55:59 -0300

    Fixed rubocop errors on lib/kuniri/state_machine/OO_structured_fsm/module_state.rb
    Signed-off-by: Bruno <bruno.sesso@usp.br>
    Signed-off-by: Lucas Kanashiro <kanashiro@riseup.net>

--- lib/kuniri/state_machine/OO_structured_fsm/module_state.rb
+++ lib/kuniri/state_machine/OO_structured_fsm/module_state.rb
@@ -15,9 +15,6 @@ module StateMachine
 
     # Class responsible for handling Module state.
     class ModuleState < OOStructuredState
-
-      @language
-
       def initialize(pLanguage)
         @language = pLanguage
       end
@@ -61,22 +58,14 @@ module StateMachine
 
         if @language.endBlockHandler.has_end_of_block?(pLine)
           previous = @language.previousState.last
-
-          if (previous.is_a?(StateMachine::OOStructuredFSM::IdleState))
-            idle_capture
-          else
-            return pElementFile
-          end
+          idle_state = StateMachine::OOStructuredFSM::IdleState
+          return pElementFile unless previous.is_a? idle_state
+          idle_capture
         end
 
         return pElementFile
       end
 
-    # End class
-    end
-
-  # End OOStructuredFSM
-  end
-
-# End StateMachine
-end
+    end # End class
+  end # End OOStructuredFSM
+end # End StateMachine
