099ca20079134d40d358ae43f3c79414ffab298f
Bruno <bruno.sesso@usp.br>
2017-04-05 15:00:30 -0300

    Fixed rubocop errors on lib/kuniri/language/abstract_container/structured_and_oo/aggregation.rb
    Signed-off-by: Bruno <bruno.sesso@usp.br>
    Signed-off-by: Lucas Kanashiro <kanashiro@riseup.net>

--- lib/kuniri/language/abstract_container/structured_and_oo/aggregation.rb
+++ lib/kuniri/language/abstract_container/structured_and_oo/aggregation.rb
@@ -8,28 +8,22 @@ module Languages
 
   # @abstract Aggregation is a container for handling aggregation among classes
   class Aggregation
-
-    public
-
-      # Verify if line has an aggregation.
-      # @param pLine Line with the potential class.
-      # @return Return an object AggregationData if it find a class in the
-      #         line, otherwise return nil.
-      def get_aggregation(pLine)
-        raise NotImplementedError
-      end
+    # Verify if line has an aggregation.
+    # @param pLine Line with the potential class.
+    # @return Return an object AggregationData if it find a class in the
+    #         line, otherwise return nil.
+    def get_aggregation(_pLine)
+      raise NotImplementedError
+    end
 
     protected
 
-      # Detect if an aggregation exists.
-      # @param pLine Line to inspect.
-      # @return Returns an row string, otherwise returns nil.
-      def detect_aggregation(pLine)
-        raise NotImplementedError
-      end
+    # Detect if an aggregation exists.
+    # @param pLine Line to inspect.
+    # @return Returns an row string, otherwise returns nil.
+    def detect_aggregation(_pLine)
+      raise NotImplementedError
+    end
 
-  # class
-  end
-
-# module
-end
+  end # class
+end # module
