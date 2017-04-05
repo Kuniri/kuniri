ad2a6653ed6901fe6cf66fc0233291a5e3ec2ac7
Bruno <bruno.sesso@usp.br>
2017-04-05 15:46:34 -0300

    Fixed rubocop errors on lib/kuniri/language/abstract_container/structured_and_oo/extern_requirement.rb
    Signed-off-by: Bruno <bruno.sesso@usp.br>
    Signed-off-by: Lucas Kanashiro <kanashiro@riseup.net>

--- lib/kuniri/language/abstract_container/structured_and_oo/extern_requirement.rb
+++ lib/kuniri/language/abstract_container/structured_and_oo/extern_requirement.rb
@@ -10,33 +10,26 @@ module Languages
   # An external requirement is the "#include" in C, "require" in Ruby,
   # "import" in Java, etc.
   class ExternRequirement
-
-    public
-
-      # Get requirement name.
-      # @param pLine Line to be analysed.
-      # @return Return the ExternRequirementData object, or nil.
-      def get_requirement(pLine)
-        raise NotImplementedError
-      end
+    # Get requirement name.
+    # @param pLine Line to be analysed.
+    # @return Return the ExternRequirementData object, or nil.
+    def get_requirement(_pLine)
+      raise NotImplementedError
+    end
 
     protected
 
-      # @param pLine Line to detect requirement.
-      # @return Returns a string with row extern requirement, or nil if not
-      #         detects anything.
-      def detect_extern_requirement(pLine)
-        raise NotImplementedError
-      end
+    # @param pLine Line to detect requirement.
+    # @return Returns a string with row extern requirement, or nil if not
+    #         detects anything.
+    def detect_extern_requirement(_pLine)
+      raise NotImplementedError
+    end
 
-      # @param pLine Line with row string to be pruned.
-      # @return Returns an string.
-      def remove_unnecessary_information(pLine)
-        raise NotImplementedError
-      end
-
-  # Class
-  end
-
-# Module
-end
+    # @param pLine Line with row string to be pruned.
+    # @return Returns an string.
+    def remove_unnecessary_information(_pLine)
+      raise NotImplementedError
+    end
+  end # Class
+end # Module
