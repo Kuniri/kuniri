070278c8c4b2e338010e209e3bda4f95c3bca7be
Bruno <bruno.sesso@usp.br>
2017-04-06 10:42:26 -0300

    Fixed rubocop errors on lib/kuniri/language/container_data/stucturued_and_oo/function_data.rb
    Signed-off-by: Bruno <bruno.sesso@usp.br>
    Signed-off-by: Lucas Kanashiro <kanashiro@riseup.net>

--- lib/kuniri/language/container_data/structured_and_oo/function_data.rb
+++ lib/kuniri/language/container_data/structured_and_oo/function_data.rb
@@ -10,18 +10,11 @@ module Languages
 
   # Handle function data
   class FunctionData < Languages::FunctionAbstract
-
-    public
-
-      # Initialize function data based on name.
-      # @param pFunctionName name string.
-      def initialize(pFunctionName)
-        super(pFunctionName)
-        @type = GLOBAL_FUNCTION_DATA
-      end
-
-  # class
-  end
-
-# Module
-end
+    # Initialize function data based on name.
+    # @param pFunctionName name string.
+    def initialize(pFunctionName)
+      super(pFunctionName)
+      @type = GLOBAL_FUNCTION_DATA
+    end
+  end # class
+end # Module
