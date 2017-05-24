c7096fabb996839f6539f0f804afe28780852009
Bernardo Amorim <bsandinoamorim@gmail.com>
2017-05-24 16:01:39 -0300

    Implement multiple-line statement zipping
    Signed-off-by: Bruno Sesso <bruno.sesso@usp.br>
    Signed-off-by: Bernardo Amorim <bsandinoamorim@gmail.com>

--- lib/kuniri/preparser/ruby.rb
+++ lib/kuniri/preparser/ruby.rb
@@ -1,7 +1,12 @@
 require_relative 'preparser.rb'
 
+
+
 class RubyPreParser < PreParser
+
+  
   def initialize
+    @LINE_CONTINUATION_CHARS = ['.', ',']
     @on_multiline_comment = false
   end
 
@@ -18,7 +23,26 @@ class RubyPreParser < PreParser
     return pre_parsed_line
   end
 
-  def zip_multiple_lines_command(line) end
+  def zip_multiple_lines_command(lines)
+    zipped_lines = []
+    curr_line = ""
+    append_next = true
+    for line in lines
+      if append_next
+        curr_line << line
+      else
+        zipped_lines.push(curr_line)
+        curr_line = line
+      end
+      if @LINE_CONTINUATION_CHARS.include?(line[-1])
+        append_next = true
+      else
+        append_next = false
+      end
+    end
+    zipped_lines.push(curr_line)
+    return zipped_lines
+  end
 
   def split_multiple_command_line(line) end
 end
