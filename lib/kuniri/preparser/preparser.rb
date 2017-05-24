73819ca60e49fc47afaaf8c089a177dada923691
Bernardo Amorim <bsandinoamorim@gmail.com>
2017-05-24 14:32:28 -0300

    Implement single-line and multiple-line pre-parsing
    Signed-off-by: Bruno Sesso <bruno.sesso@usp.br>
    Signed-off-by: Bernardo Amorim <bsandinoamorim@gmail.com>

--- lib/kuniri/preparser/preparser.rb
+++ lib/kuniri/preparser/preparser.rb
@@ -1,18 +1,47 @@
 class PreParser
   def pre_parse(source, out_path)
     pre_parsed_lines = []
-    for line in source
-      pre_parsed_line = remove_comments(line)
-      pre_parsed_line.slice!("\n")
+    source.each do |line|
+      pre_parsed_line = pre_parse_line(line)
       pre_parsed_lines.push(pre_parsed_line) unless (pre_parsed_line.empty?)
     end
-    out_file = File.open(out_path, "w")
-    for pre_parsed_line in pre_parsed_lines
-      out_file.write(pre_parsed_lines)      
-    end
+    pre_parse_multiple_lines(pre_parsed_lines)
+    out_file = File.open(out_path, 'w')
+    out_file.write(pre_parsed_lines.join("\n"))
+    out_file.close
   end
 
-  def remove_comments(line)
+  def pre_parse_line(pre_parsed_line)
+    pre_parsed_line = remove_whitespaces(pre_parsed_line)
+    pre_parsed_line = remove_newline(pre_parsed_line)
+    return pre_parsed_line
+  end
+
+  def pre_parse_multiple_lines(lines)
+    lines = zip_multiple_lines_command(lines)
+    lines = split_multiple_command_line(lines)
+    return lines
+  end
+
+  def split_multiple_command_line(_lines)
     raise NotImplementedError
+  end
+
+  def zip_multiple_lines_command(_lines)
+    raise NotImplementedError
+  end
+
+  def remove_whitespaces(line)
+    return line.strip
+  end
+
+  def remove_comments(_line)
+    raise NotImplementedError
+  end
+
+  def remove_newline(line)
+    no_newline = line
+    no_newline.slice("\n")
+    return no_newline
   end
 end
