0cac9733d896c88c9a70df1aa85a1849229539d4
Bernardo Amorim <bsandinoamorim@gmail.com>
2017-05-24 14:35:18 -0300

    Comply file to rubocop
    Signed-off-by: Bruno Sesso <bruno.sesso@usp.br>
    Signed-off-by: Bernardo Amorim <bsandinoamorim@gmail.com>

--- lib/kuniri/preparser/ruby.rb
+++ lib/kuniri/preparser/ruby.rb
@@ -4,20 +4,21 @@ class RubyPreParser < PreParser
   def initialize
     @on_multiline_comment = false
   end
+
   def remove_comments(line)
-    if line =~ /^=begin(.*?)/
-      @on_multiline_comment = true
-    end
+    @on_multiline_comment = true if line =~ /^=begin(.*?)/
     if @on_multiline_comment
       pre_parsed_line = ''
     else
       comment_regex = /(?<non_comm>[^#]*)#.*/
       match = line.match(comment_regex)
-      pre_parsed_line = if not match.nil? then match["non_comm"] else line end
+      pre_parsed_line = match.nil? ? line : match['non_comm']
     end
-    if line =~ /^=end/
-      @on_multiline_comment = false
-    end
+    @on_multiline_comment = false if line =~ /^=end/
     return pre_parsed_line
   end
+
+  def zip_multiple_lines_command(line) end
+
+  def split_multiple_command_line(line) end
 end
