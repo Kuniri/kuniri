47f375b88c3e0fb70f99911f947df9d272910de6
Bernardo Amorim <bsandinoamorim@gmail.com>
2017-05-25 11:35:58 -0300

    Comply file to rubocop and add parenthesis as line-zipping char
    Signed-off-by: Bruno Sesso <bruno.sesso@usp.br>
    Signed-off-by: Bernardo Amorim <bsandinoamorim@gmail.com>
    Signed-off-by: Lucas Kanashiro <kanashiro@debian.org>
    Signed-off-by: Giuliano Belinassi <giuliano.belinassi@usp.br>

--- lib/kuniri/preparser/ruby.rb
+++ lib/kuniri/preparser/ruby.rb
@@ -1,12 +1,9 @@
 require_relative 'preparser.rb'
 
-
-
 class RubyPreParser < PreParser
 
-  
   def initialize
-    @LINE_CONTINUATION_CHARS = ['.', ',']
+    @LINE_CONTINUATION_CHARS = ['.', ',', '(']
     @on_multiline_comment = false
   end
 
@@ -25,26 +22,22 @@ class RubyPreParser < PreParser
 
   def zip_multiple_lines_command(lines)
     zipped_lines = []
-    curr_line = ""
+    curr_line = ''
     append_next = true
-    for line in lines
+    lines.each do |line|
       if append_next
         curr_line << line
       else
         zipped_lines.push(curr_line)
         curr_line = line
       end
-      if @LINE_CONTINUATION_CHARS.include?(line[-1])
-        append_next = true
-      else
-        append_next = false
-      end
+      append_next = @LINE_CONTINUATION_CHARS.include?(line[-1])
     end
     zipped_lines.push(curr_line)
     return zipped_lines
   end
 
-  def split_multiple_command_line(line)
-    return line
+  def split_multiple_command_lines(lines)
+    return lines
   end
 end
